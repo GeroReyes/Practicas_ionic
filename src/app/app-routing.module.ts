1a57d952989ec9fe1a2611393011ca00fd8d383e
GeroReyes <geroreyes7789@gmail.com>
2025-01-20 20:12:07 -0600

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,70 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'menu',
+    loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule)
+  },
+  {
+    path: 'cards',
+    loadChildren: () => import('./cards/cards.module').then( m => m.CardsPageModule)
+  },
+  {
+    path: 'bad',
+    loadChildren: () => import('./bad/bad.module').then( m => m.BadPageModule)
+  },
+  {
+    path: 'chips',
+    loadChildren: () => import('./chips/chips.module').then( m => m.ChipsPageModule)
+  },
+  {
+    path: 'date',
+    loadChildren: () => import('./date/date.module').then( m => m.DatePageModule)
+  },
+  {
+    path: 'modal',
+    loadChildren: () => import('./modal/modal.module').then( m => m.ModalPageModule)
+  },
+  {
+    path: 'ionmenu',
+    loadChildren: () => import('./ionmenu/ionmenu.module').then( m => m.IonmenuPageModule)
+  },
+  {
+    path: 'search',
+    loadChildren: () => import('./search/search.module').then( m => m.SearchPageModule)
+  },
+  {
+    path: 'toast',
+    loadChildren: () => import('./toast/toast.module').then( m => m.ToastPageModule)
+  },
+  {
+    path: 'tool',
+    loadChildren: () => import('./tool/tool.module').then( m => m.ToolPageModule)
+  },
+  {
+    path: 'button',
+    loadChildren: () => import('./button/button.module').then( m => m.ButtonPageModule)
+  },
+  {
+    path: 'registre',
+    loadChildren: () => import('./registre/registre.module').then( m => m.RegistrePageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/cartas/cartas.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cartas',
+  templateUrl: './cartas.page.html',
+  styleUrls: ['./cartas.page.scss'],
+  standalone: false,
+})
+export class CartasPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
